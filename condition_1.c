50393cf309a317a4f4875e1f04a3de86028ee91f
Mehedi Hasan <Mohammadmehedi1250@gmail.com>
2023-04-03 16:43:01 +0600

    init

---
+++ condition_1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+int main (){
+
+   
+   int number ;
+   printf("Enter your ID :");
+   scanf("%d",&number);
+
+
+   if (number == 2085)
+   {
+    printf("Welcome to Green University");
+   }
+   else{
+
+    printf("Enter a Valid ID");
+   }
+
+return 0 ;
+
+
+
+}
