0cea9bcaf5d57588ab1eb0f41a57a4a06b9d3218
Mehedi Hasan <Mohammadmehedi1250@gmail.com>
2023-04-03 16:56:28 +0600

    init

---
+++ condition_2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+
+int main (){
+
+   
+   int number ;
+   printf("Enter your ID :");
+   scanf("%d",&number);
+
+
+   if (number == 2085)
+   {
+    printf("Welcome to Green University");
+   }
+   else if (number == 2078)
+   {
+    printf("Welcome to Green University");
+   }
+  else
+  {
+    printf("Enter a Valid ID");
+  }
+  
+
+
+
+return 0 ;
+
+
+
+}
---
+++ condition_3.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+
+int main (){
+
+   
+   int number ,payment;
+   printf("Enter your ID :");
+   scanf("%d",&number);
+   printf("Enter your PAYMENT ID :");
+   scanf("%d",&payment);
+
+
+   if (number >= 2085)
+   {
+    printf("Welcome to Green University\n");
+
+      if (payment == 1010 )
+      {
+        printf("Welcome To You're ID\n");
+      }
+      else{
+
+        printf("Payment FIRST");
+      }
+
+
+   }
+   else{
+
+    printf("Enter a Valid ID");
+   }
+
+return 0 ;
+
+
+
+}
