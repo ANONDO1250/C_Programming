7b36246b39bef57f237c84d72920a9e314ef9eba
Mehedi Hasan <Mohammadmehedi1250@gmail.com>
2023-04-05 04:01:16 +0600

    init

---
+++ LOOP_iloveyou.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main (){
+
+      int num,quantity;
+      
+      
+      printf("Enter your Starting Number : ");
+      scanf("%d",&num);
+      printf(" Enter Your messege qantity demand : \n");
+    scanf("%d ", &quantity);
+      
+
+
+      for ( num; num <= quantity; num++)
+      {
+        printf("%d I love You Asha\n", num);
+      }
+      
+      return 0;
+
+
+
+
+
+
+}
